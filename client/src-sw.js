4742a79f033bd1d54768be28a1cd791b053b62ba
kevinverduzco27 <kevin.verduzco27@gmail.com>
2023-08-24 18:11:08 -0700

    finished the config for this

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,20 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 7 * 24 * 60 * 60, // Adjust as needed
+    }),
+  ],
+});
+
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'image',
+  assetCache
+);
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,42 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault(); // Prevent the default prompt
+  deferredPrompt = event; // Store the event for later use
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // Update the UI to show the install button or related UI element
+  butInstall.style.display = 'block';
+});
+
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    // Show the installation prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    const choiceResult = await deferredPrompt.userChoice;
+
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the installation prompt.');
+    } else {
+      console.log('User dismissed the installation prompt.');
+    }
+
+    // Reset the deferred prompt variable
+    deferredPrompt = null;
+
+    // Hide the install button
+    butInstall.style.display = 'none';
+  }
+});
+
+// Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed successfully.');
+  // You can add additional actions here if needed
+});
